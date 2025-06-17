220e221cd086a658fe18c1227fea6f0563f4fd86
Siddu4101 <siddu412001@gmail.com>
2025-06-17 22:44:37 +0530

    adding explanation for prime number check

--- src/main/java/org/example/dsa/questions/PrimeNumber.java
+++ src/main/java/org/example/dsa/questions/PrimeNumber.java
@@ -8,6 +8,11 @@ public class PrimeNumber {
         System.out.println(checkPrimeNumber(new Scanner(System.in).nextInt()));
     }
 
+    /*
+        If a number N is not prime, it can be expressed as the product of two factors: [ N = a times b ] Now,
+        if both a and b were greater than sqrt(N), their product would exceed N. This means that at least
+        one of the factors must be less than or equal to sqrt(N).
+    */
     private static boolean checkPrimeNumber(int number) {
         if (number <= 1)
             return false;
