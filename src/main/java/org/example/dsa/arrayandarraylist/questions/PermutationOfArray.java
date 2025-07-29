723feb33c90013b72d87ab9741239ad9c078df6a
Siddu4101 <siddu412001@gmail.com>
2025-07-30 00:42:48 +0530

    permutation of an array updated question

--- src/main/java/org/example/dsa/arrayandarraylist/questions/PermutationOfArray.java
+++ src/main/java/org/example/dsa/arrayandarraylist/questions/PermutationOfArray.java
@@ -7,7 +7,10 @@ public class PermutationOfArray {
 
     /*
      * Q:https://leetcode.com/problems/build-array-from-permutation/
-     *`nput: nums = [0,2,1,5,3,4]
+     * Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]]
+     * for each 0 <= i < nums.length and return it.
+     * A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+     *Input: nums = [0,2,1,5,3,4]
         Output: [0,1,2,4,5,3]
         Explanation: The array ans is built as follows:
         ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
