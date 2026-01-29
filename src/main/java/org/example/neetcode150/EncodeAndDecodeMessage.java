d08ec6398810d2c44a21ebd9704a760db6a1b00a
Siddu4101 <siddu412001@gmail.com>
2026-01-30 01:01:47 +0530

    6. Encode and decode the message

---
+++ src/main/java/org/example/neetcode150/EncodeAndDecodeMessage.java
@@ -0,0 +1,70 @@
+package org.example.neetcode150;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class EncodeAndDecodeMessage {
+
+    /*
+     * Q:https://neetcode.io/problems/string-encode-and-decode/question?list=neetcode150(leetcode needs subscription for this question so doing it from neetcode)
+     *
+     *Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+        Machine 1 (sender) has the function:
+
+        string encode(vector<string> strs) {
+            // ... your code
+            return encoded_string;
+        }
+        Machine 2 (receiver) has the function:
+
+        vector<string> decode(string s) {
+            //... your code
+            return strs;
+        }
+        So Machine 1 does:
+
+        string encoded_string = encode(strs);
+        and Machine 2 does:
+
+        vector<string> strs2 = decode(encoded_string);
+        strs2 in Machine 2 should be the same as strs in Machine 1.
+
+        Implement the encode and decode methods.
+
+        Example 1:
+
+        Input: dummy_input = ["Hello","World"]
+
+        Output: ["Hello","World"]
+
+        Explanation:
+        Machine 1:
+        Codec encoder = new Codec();
+        String msg = encoder.encode(strs);
+        Machine 1 ---msg---> Machine 2
+
+        Machine 2:
+        Codec decoder = new Codec();
+        String[] strs = decoder.decode(msg);
+     *
+     * */
+
+    public static void main(String[] args) {
+        List<String> input = List.of("Hello", "World");
+        String encode = encode(input);
+        log.info("Encoded data is {}", encode);
+        log.info("Decoded data is {}", decode(encode));
+    }
+
+    public static String encode(List<String> strs) {
+        return String.join("|", strs);
+    }
+
+    public static List<String> decode(String str) {
+        return Arrays.stream(str.split("\\|")).toList();
+    }
+}
