10ebe7ee23fd7d6ef3931b04b5822073e862ccc0
Siddu4101 <siddu412001@gmail.com>
2026-01-31 13:28:14 +0530

    6. Encode and decode the message via prefix length method

--- src/main/java/org/example/neetcode150/EncodeAndDecodeMessage.java
+++ src/main/java/org/example/neetcode150/EncodeAndDecodeMessage.java
@@ -2,6 +2,7 @@ package org.example.neetcode150;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -54,19 +55,54 @@ public class EncodeAndDecodeMessage {
      * */
 
     public static void main(String[] args) {
-        List<String> input = List.of("Hello", "World");
+        List<String> input = List.of("a", "ant", "saw", "that");// List.of("Hello", "World");
         String encode = encode(input);
         log.info("Encoded data is {}", encode);
         log.info("Decoded data is {}", decode(encode));
+        String encodeViaPrefixLength = encodeViaLengthPrefix(input);
+        log.info("Encoded data from prefix length {}", encodeViaPrefixLength);
+        log.info("Encoded data is {}", decodeViaLengthPrefix(encodeViaPrefixLength));
     }
 
-    /*Approach 1*/
+    /*Approach 1 Bcz strs[i] contains any possible characters out of 256 valid ASCII characters. */
     /*here we are using the non-ascii char é as a delimiter for encode and decode*/
     public static String encode(List<String> strs) {
         return String.join("é", strs);
     }
 
     public static List<String> decode(String str) {
-        return Arrays.stream(str.split("\\é")).toList();
+        return Arrays.stream(str.split("é")).toList();
     }
+
+    /*Approach 2*/
+    /*we will use the length of the string prefix followed by a character($) for encoding
+     *Then while decoding we will search for that ($) and read the number before that and pick the number of character from the encoded string and continue...
+     * */
+    public static String encodeViaLengthPrefix(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+        for (String str : strs) {
+            encodedString.append(str.length());
+            encodedString.append("$");
+            encodedString.append(str);
+        }
+        return encodedString.toString();
+    }
+
+    public static List<String> decodeViaLengthPrefix(String encodedString) {
+        List<String> decodedStringList = new ArrayList<>();
+
+        int stringLengthCount = 0;
+        for (int i = 0; i < encodedString.length(); i++) {
+            if (encodedString.charAt(i) == '$') {
+                int stringLength = Integer.parseInt(encodedString.substring(i - stringLengthCount, i)); /*substring to get the string length number/prefix*/
+                decodedStringList.add(encodedString.substring(i + 1, stringLength + i + 1));/*read the string from encoded based on prefix length*/
+                i = i + stringLength;/*move the cursor to the next prefix count not doing +1 bcz it will be done by for post increment i++*/
+                stringLengthCount = 0; /*reset the string length/prefix length count */
+            } else {
+                stringLengthCount++;
+            }
+        }
+        return decodedStringList;
+    }
+
 }
