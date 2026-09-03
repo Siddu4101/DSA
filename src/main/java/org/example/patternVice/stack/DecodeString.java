f67a419fa13f47edef5b22654bcb85ed18e2c1cd
Siddu4101 <siddu412001@gmail.com>
2026-09-04 00:31:03 +0530

    add decodeStringBetter implementation for optimized decoding and update overview

--- src/main/java/org/example/patternVice/stack/DecodeString.java
+++ src/main/java/org/example/patternVice/stack/DecodeString.java
@@ -2,6 +2,8 @@ package org.example.patternVice.stack;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 @Slf4j
@@ -25,9 +27,24 @@ Output: "accaccacc"
     public static void main(String[] args) {
         String s = "3[a]2[bc]";
         log.info("The decoded string is {}", decodeString(s));
+        log.info("The decoded string is better approach {}", decodeStringBetter(s));
     }
 
 
+    /*
+     * Approach:
+     * 1. keep str for the string to be repeated number times
+     * 2. if incoming not a closing brace just push it to the stack if it is closing till open comes in the stack pop it to str and append reversely
+     * 3. then pop the open brace and then check the number of times this string to be repeated by popping numbers from stack
+     * 4. now repeat the str * number times and push back to the stack
+     * 5. at the end just pop all the strings and merge reversely and return
+     *
+     * */
+
+    /*
+     * T: O(n * m) -> m is the max char inside the []
+     * S: O(n)
+     * */
     public static String decodeString(String s) {
         Stack<String> stack = new Stack<>();
         StringBuilder str;
@@ -61,4 +78,42 @@ Output: "accaccacc"
         }
         return result.toString();
     }
+
+    /*
+     * Approach: 02
+     * 1. 1 stack to maintain the number and 1 for the str and the current for the final result
+     * 2. if the char is a alphabet just append it to the current
+     * 3. if the incoming char is number convert this to the number
+     * 4. if it open brace push it to the str stack and the count of it in the count stack as a number
+     * 5. once we have closing brace pop the countStack and repeat the current str those many times and pop the str and append the previous str
+     * 6. at the end return the current
+     * */
+
+    public static String decodeStringBetter(String s) {
+        Deque<Integer> countStack = new ArrayDeque<>();
+        Deque<StringBuilder> strStack = new ArrayDeque<>();
+        StringBuilder current = new StringBuilder();
+        int num = 0;
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) { /*to create the number*/
+                num = num * 10 + (c - '0');
+            } else if (c == '[') { /* if it is a open brace just plush the count and string to stack and reset them*/
+                countStack.push(num);
+                strStack.push(current);
+                num = 0;
+                current = new StringBuilder();
+            } else if (c == ']') { /*when it is closing just pop the last pushed and count and repeat it with current and append to previous */
+                int repeat = countStack.pop();
+                StringBuilder prev = strStack.pop();
+                prev.append(current.toString().repeat(repeat));
+                current = prev;
+            } else {
+                current.append(c); /*if it is just alphabet just append to the current*/
+            }
+        }
+
+        return current.toString();
+    }
+
 }
